1fddad28aa6d7463a9dbbac5bfa22671353614e8
llewyn <kbj2060@naver.com>
2021-12-02 17:34:12 +0900

    [New Feature] modal updated

--- src/components/_dashboard/app/AppWaterSupply.js
+++ src/components/_dashboard/app/AppWaterSupply.js
@@ -1,12 +1,10 @@
+import * as React from 'react';
 import { Icon } from '@iconify/react';
 import bxWater from '@iconify/icons-bx/bx-water';
-// material
 import { alpha, styled } from '@mui/material/styles';
 import { Card, Typography } from '@mui/material';
-// utils
-import { fShortenNumber } from '../../../utils/formatNumber';
-
-// ----------------------------------------------------------------------
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 
 const RootStyle = styled(Card)(({ theme }) => ({
   boxShadow: 'none',
@@ -32,13 +30,27 @@ const IconWrapperStyle = styled('div')(({ theme }) => ({
   )} 100%)`
 }));
 
-// ----------------------------------------------------------------------
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+};
 
 const TOTAL = 714000;
 
 export default function AppWaterSupply() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <RootStyle>
+    <RootStyle onClick={handleOpen}>
       <IconWrapperStyle>
         <Icon icon={bxWater} width={24} height={24} />
       </IconWrapperStyle>
@@ -46,6 +58,21 @@ export default function AppWaterSupply() {
       <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
         물공급량
       </Typography>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </RootStyle>
   );
 }
