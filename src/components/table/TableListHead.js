85f963c0e6ed899db6ae59274c3f3d089324a57c
kbj2060 <kbj2060@naver.com>
2021-12-30 23:39:07 +0900

    [New Styles] tablehead chagned

--- src/components/table/TableListHead.js
+++ src/components/table/TableListHead.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TableRow, TableCell, TableHead, TableSortLabel } from '@mui/material';
+import { TableRow, TableCell, TableHead, styled } from '@mui/material';
+import { tableCellClasses } from '@mui/material/TableCell';
 
 TableListHead.propTypes = {
   headLabel: PropTypes.array
 };
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.darker,
+    color: theme.palette.common.white
+  }
+}));
 
 export default function TableListHead({ headLabel }) {
   return (
     <TableHead>
       <TableRow>
         {headLabel.map((headCell) => (
-          <TableCell align="center" key={headCell.id}>
-            <TableSortLabel>{headCell.label}</TableSortLabel>
-          </TableCell>
+          <StyledTableCell sx={{ padding: '10px' }} align="right" key={headCell.id}>
+            <TableRow sx={{ display: 'flex', justifyContent: 'center' }}>{headCell.label}</TableRow>
+          </StyledTableCell>
         ))}
       </TableRow>
     </TableHead>
